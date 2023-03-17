300783ef31767ca56a6d8d3fe2bb7e253d0ea401
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-03-17 18:35:51 +0500

    Add linters and fix errors

--- javascript.js
+++ javascript.js
@@ -1,73 +1,69 @@
 // mobile menu section
 function myFunction() {
-  document.getElementById("mobile-menu").style.visibility = "visible";
-  document.getElementById("menu-button").style.visibility = "hidden";
+  document.getElementById('mobile-menu').style.visibility = 'visible';
+  document.getElementById('menu-button').style.visibility = 'hidden';
 }
-
-// myFunction();
 
 function myFunction2() {
-  document.getElementById("mobile-menu").style.visibility = "hidden";
-  document.getElementById("menu-button").style.visibility = "visible";
+  document.getElementById('mobile-menu').style.visibility = 'hidden';
+  document.getElementById('menu-button').style.visibility = 'visible';
 }
-
-// myFunction2();
 
 // projects section with popup window
 
 const array = [
   {
     number: 0,
-    heading: "Tonic",
+    heading: 'Tonic',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    image: "images/Snapshoot Portfolio1.png",
-    popupImage: "images/popup1.png",
-    languages: ["html", "css", "javascript", "Ruby", "GitHub", "Bootstrap"],
-    profession: ["Canopy", "Full Stack Dev", "2015"],
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: 'images/Snapshoot Portfolio1.png',
+    popupImage: 'images/popup1.png',
+    languages: ['html', 'css', 'javascript', 'Ruby', 'GitHub', 'Bootstrap'],
+    profession: ['Canopy', 'Full Stack Dev', '2015'],
     popupDescription:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and when an unknown printer took a galley of type and and when an  unknown printer took a galley of type and scrambled it 1960s",
   },
   {
     number: 1,
-    heading: "Multi-Post Stories",
+    heading: 'Multi-Post Stories',
     description:
-      "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
-    image: "images/Snapshoot Portfolio2.png",
-    popupImage: "images/popup2.png",
-    languages: ["html", "css", "javascript", "Ruby", "GitHub", "Bootstrap"],
-    profession: ["FACEBOOK", "Full Stack Dev", "2015"],
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    image: 'images/Snapshoot Portfolio2.png',
+    popupImage: 'images/popup2.png',
+    languages: ['html', 'css', 'javascript', 'Ruby', 'GitHub', 'Bootstrap'],
+    profession: ['FACEBOOK', 'Full Stack Dev', '2015'],
     popupDescription:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and when an unknown printer took a galley of type and scrambled it 1960s",
   },
   {
     number: 2,
-    heading: "Facebook 360",
+    heading: 'Facebook 360',
     description:
       "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    image: "images/Snapshoot Portfolio3.png",
-    popupImage: "images/popup3.png",
-    languages: ["html", "css", "javascript", "Ruby", "GitHub", "Bootstrap"],
-    profession: ["FACEBOOK", "Full Stack Dev", "2015"],
+    image: 'images/Snapshoot Portfolio3.png',
+    popupImage: 'images/popup3.png',
+    languages: ['html', 'css', 'javascript', 'Ruby', 'GitHub', 'Bootstrap'],
+    profession: ['FACEBOOK', 'Full Stack Dev', '2015'],
     popupDescription:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and when an unknown printer took a galley of type and scrambled it 1960s",
   },
   {
     number: 3,
-    heading: "Uber Navigation",
+    heading: 'Uber Navigation',
     description:
-      "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
-    image: "images/Snapshoot Portfolio4.png",
-    popupImage: "images/popup4.png",
-    languages: ["html", "css", "javascript", "Ruby", "GitHub", "Bootstrap"],
-    profession: ["Uber", "Lead Developer", "2018"],
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    image: 'images/Snapshoot Portfolio4.png',
+    popupImage: 'images/popup4.png',
+    languages: ['html', 'css', 'javascript', 'Ruby', 'GitHub', 'Bootstrap'],
+    profession: ['Uber', 'Lead Developer', '2018'],
     popupDescription:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and when an unknown printer took a galley of type and scrambled it 1960s",
   },
 ];
 
 function project() {
-  const a = document.getElementById("project-portfolio");
+  const a = document.getElementById('project-portfolio');
   for (let i = 0; i < array.length; i += 1) {
     if (i % 2 === 0) {
       a.innerHTML += `
@@ -137,33 +133,30 @@ function project() {
   }
 }
 
-// project();
-
 function removeBlur() {
-  document.getElementById("header").style.filter = "blur(0px)";
-  document.getElementById("headline").style.filter = "blur(0px)";
-  document.getElementById("about").style.filter = "blur(0px)";
-  document.getElementById("contact").style.filter = "blur(0px)";
-  document.getElementById("project-portfolio").style.filter = "blur(0px)";
+  document.getElementById('header').style.filter = 'blur(0px)';
+  document.getElementById('headline').style.filter = 'blur(0px)';
+  document.getElementById('about').style.filter = 'blur(0px)';
+  document.getElementById('contact').style.filter = 'blur(0px)';
+  document.getElementById('project-portfolio').style.filter = 'blur(0px)';
 }
 
 function closePopup() {
-  document.getElementById("popup").style.display = "none";
+  document.getElementById('popup').style.display = 'none';
   removeBlur();
-  document.getElementById("header").style.position = "fixed";
+  document.getElementById('header').style.position = 'fixed';
 }
-// closePopup();
 
 function makeBlur() {
-  document.getElementById("header").style.filter = "blur(10px)";
-  document.getElementById("headline").style.filter = "blur(10px)";
-  document.getElementById("about").style.filter = "blur(10px)";
-  document.getElementById("contact").style.filter = "blur(10px)";
-  document.getElementById("project-portfolio").style.filter = "blur(10px)";
+  document.getElementById('header').style.filter = 'blur(10px)';
+  document.getElementById('headline').style.filter = 'blur(10px)';
+  document.getElementById('about').style.filter = 'blur(10px)';
+  document.getElementById('contact').style.filter = 'blur(10px)';
+  document.getElementById('project-portfolio').style.filter = 'blur(10px)';
 }
 
 function popup(number) {
-  const b = document.getElementById("popup-window");
+  const b = document.getElementById('popup-window');
   makeBlur();
 
   b.innerHTML = `
@@ -219,29 +212,27 @@ function popup(number) {
     </div>
 
     `;
-  document.getElementById("header").style.position = "static";
+  document.getElementById('header').style.position = 'static';
 }
-
-// popup();
 
 // Preserve data in the browser
 
-const form = document.getElementById("form-id");
-const nameInput = document.getElementById("username");
-const message = document.getElementById("message");
-const emailInput = document.getElementById("useremail");
+const form = document.getElementById('form-id');
+const nameInput = document.getElementById('username');
+const message = document.getElementById('message');
+const emailInput = document.getElementById('useremail');
 
-form.addEventListener("input", () => {
+form.addEventListener('input', () => {
   const formInfo = {
     name: nameInput.value,
     email: emailInput.value,
     message: message.value,
   };
-  localStorage.setItem("formDetails", JSON.stringify(formInfo));
+  localStorage.setItem('formDetails', JSON.stringify(formInfo));
 });
 
 function displayInfo() {
-  const info = JSON.parse(localStorage.getItem("formDetails"));
+  const info = JSON.parse(localStorage.getItem('formDetails'));
   if (info) {
     nameInput.value = info.name;
     emailInput.value = info.email;
